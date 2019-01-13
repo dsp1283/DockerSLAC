f7ca7c8e37fe592642c37e7a22b80802bc823f82
dsp1283 <46639301+dsp1283@users.noreply.github.com>
2019-01-12 22:22:56 -0500

    Add files via upload

---
+++ Lab1/Sample Docker Python/Add.py
@@ -0,0 +1,10 @@
+# This program adds two numbers
+
+num1 = input('What is your first value?\n')
+num2 = input('What is your second value?\n')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
